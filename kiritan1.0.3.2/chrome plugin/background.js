6f7e164c1d949cc83cd9e5b5f17ed5e3292eddd8
yide <a8849288@hotmail.com>
2019-02-01 20:50:40 +0900

    Add files via upload

---
+++ kiritan1.0.3.2/chrome plugin/background.js
@@ -0,0 +1,51 @@
+﻿var port = null;
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.type == "launch") {
+            connectToNativeHost(request.message);
+        }
+        return true;
+    }
+);
+
+
+function onDisconnencted() {
+    port = null;
+    console.log(chrome.runtime.lastError);
+}
+
+function onNativeMessage(message) {
+    console.log('recieved message from native app: ' + JSON.stringify(message));
+}
+
+//connect to native host and get the communicatetion port
+function connectToNativeHost(msg) {
+    var nativeHostName = "kiritan";
+    //Regestry 和 Native Messaging 的app名
+
+    console.log(nativeHostName);
+    if (port == null) {
+        port = chrome.runtime.connectNative(nativeHostName);
+        port.onMessage.addListener(onNativeMessage);
+        port.onDisconnect.addListener(onDisconnencted);
+    }
+    else
+        console.log("輸出中");
+    port.postMessage({ message: msg });
+} 
+
+chrome.contextMenus.create(
+    {
+        "title": "きりたん読む",
+        "type": "normal",
+        "id": "kiritan",
+        "contexts": ["selection"],
+        "onclick": getdata()
+    }
+);
+function getdata() {
+    //info 頁面資訊 tab tab上的資料
+    return function (info, tab) {
+        connectToNativeHost(info.selectionText );
+    };
+}
---
+++ kiritan1.0.3.2/chrome plugin/content.js
@@ -0,0 +1,16 @@
+﻿// JavaScript source code
+var launch_msg;
+//myCustomEvent web上的jsfunction event Name
+document.addEventListener('myCustomEvent', function (evt) {
+    chrome.runtime.sendMessage({ type: "launch", message: evt.detail }, function (response) {
+        console.log(response)
+    });
+}, false);
+run();
+function run() {
+    if (document.getElementsByClassName("js-stream-item stream-item stream-item").length != 0) {
+        alert
+        document.getElementsByClassName("js-stream-item stream-item stream-item")[0].outerHTML;
+    }
+    setTimeout(run,1000);
+}
